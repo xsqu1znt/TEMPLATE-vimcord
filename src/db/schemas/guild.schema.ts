5574a816d97a06f08cdd54adf057df85433f4fa5
squ1dggly <fluffyspiderstudios@gmail.com>
2026-02-26 17:15:50 -1000

    scaff

--- src/db/schemas/guild.schema.ts
+++ src/db/schemas/guild.schema.ts
@@ -3,13 +3,13 @@ import { createMongoSchema } from "vimcord";
 export interface IGuild {
     guildId: string;
     prefix: string | null;
-    createdAt: number;
+    createdAt: Date;
 }
 
 export const GuildSchema = createMongoSchema<IGuild>("Guilds", {
     guildId: { type: String, unique: true, required: true },
     prefix: { type: String, default: null },
-    createdAt: { type: Number, default: Date.now }
+    createdAt: { type: Date, default: () => new Date() }
 });
 
 GuildSchema.schema.index({ guildId: 1, prefix: 1 }, { unique: true });
