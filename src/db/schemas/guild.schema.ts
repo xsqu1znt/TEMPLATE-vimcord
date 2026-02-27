b860780d95fc002186221e1145330b47735ddbd5
squ1dggly <fluffyspiderstudios@gmail.com>
2026-02-26 20:26:28 -1000

    updated vimcord and mongo schema snippet

--- src/db/schemas/guild.schema.ts
+++ src/db/schemas/guild.schema.ts
@@ -2,14 +2,16 @@ import { createMongoSchema } from "vimcord";
 
 export interface IGuild {
     guildId: string;
-    prefix: string | null;
-    createdAt: Date;
+    prefix?: string;
 }
 
-export const GuildSchema = createMongoSchema<IGuild>("Guilds", {
-    guildId: { type: String, unique: true, required: true },
-    prefix: { type: String, default: null },
-    createdAt: { type: Date, default: () => new Date() }
-});
+export const GuildSchema = createMongoSchema<IGuild>(
+    "Guilds",
+    {
+        guildId: { type: String, unique: true, required: true },
+        prefix: { type: String }
+    },
+    { timestamps: true }
+);
 
 GuildSchema.schema.index({ guildId: 1, prefix: 1 }, { unique: true });
